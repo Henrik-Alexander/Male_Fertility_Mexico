610e38d1c2a85fc51d21360e682e0748dd7da192
Schubert <schubert@demogr.mpg.de>
2023-05-26 08:15:15 +0200

    Upload the META file.

---
+++ META.R
@@ -0,0 +1,16 @@
+### Meta - File
+
+
+# 1. Load the birth data
+source("Code/1-Loading.R")
+
+# 2. Impute the births
+#source("Code/2-Impute_births.R")
+
+# 3. Load the population data
+source("Code/3-Loading_Pop.R")
+
+# 4. Analysis
+source("Code/3-Analysis.R")
+
+
